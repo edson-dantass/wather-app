1999b473d446948396c9f26994bd1bba3f6e1a15
edsonjunior <edsonjunior9137@gmail.com>
2021-10-29 11:21:05 -0300

    Ajustes

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,2 @@
 export const LIBRARIES: ("drawing" | "geometry" | "localContext" | "places" | "visualization")[] = ["places"];
-export const MAP_API_KEY: string = "AIzaSyCoiE5NQisVhPjD3Lk3NLkII1GoL6IRp1Q";
+export const MAP_API_KEY: string = "";
